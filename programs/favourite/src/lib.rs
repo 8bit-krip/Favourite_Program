d76cdeb8fbfd846da8d13f67950b4759d15356db
8bit-krip <kishanpalp08@gmail.com>
2025-12-12 21:22:08 +0530

    write the test for it

--- programs/favourite/src/lib.rs
+++ programs/favourite/src/lib.rs
@@ -2,15 +2,74 @@ use anchor_lang::prelude::*;
 
 declare_id!("G9LkvYjuoF8voAr1TZ8ha1nnq1ixZPnbK6yJ9qGRnocF");
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod favourite {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn set_favourite(
+        ctx: Context<SetFavourite>,
+        number: u64,
+        colour: String,
+        hobbies: Vec<String>,
+        animal: String,
+    ) -> Result<()> {
+
+        
+        msg!("Program ID: {}", ctx.program_id);
+
+        let user_public_key = ctx.accounts.user.key();
+
+        msg!(
+            "User {} has favourite number {}, favourite colour {}, hobbies {:?}, and favourite animal {}",
+            user_public_key,
+            number,
+            colour,
+            hobbies,
+            animal
+        );
+
+        
+        ctx.accounts.favourite_account.set_inner(FavouriteData {
+            number,
+            colour,
+            hobbies,
+            animal,
+        });
+
         Ok(())
     }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct FavouriteData {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub colour: String,
+
+    #[max_len(5,50)]
+    pub hobbies: Vec<String>, 
+
+    #[max_len(50)]
+    pub animal: String,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SetFavourite<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + FavouriteData::INIT_SPACE,
+        seeds = [b"IITROORKEE", user.key().as_ref()],
+        bump
+    )]
+    pub favourite_account: Account<'info, FavouriteData>,
+
+    pub system_program: Program<'info, System>,
+}
--- tests/favourite.ts
+++ tests/favourite.ts
@@ -2,15 +2,59 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Favourite } from "../target/types/favourite";
 
-describe("favourite", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("favourite program test", () => {
 
-  const program = anchor.workspace.favourite as Program<Favourite>;
+  
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.Favourite as Program<Favourite>;
+
+  it("Sets favourite values successfully", async () => {
+
+    const user = provider.wallet.publicKey;
+
+    
+    const [favouritePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("IITROORKEE"),
+        user.toBuffer(),
+      ],
+      program.programId
+    );
+
+    console.log("Favourite PDA:", favouritePda.toBase58());
+
+    
+    const favouriteNumber = new anchor.BN(99);
+    const favouriteColour = "Blue";
+    const favouriteHobbies = ["Coding", "Gym"]; 
+    const favouriteAnimal = "Tiger";
+
+    
+    await program.methods
+      .setFavourite(
+        favouriteNumber,
+        favouriteColour,
+        favouriteHobbies,
+        favouriteAnimal
+      )
+      .accounts({
+        user: user,
+        favouriteAccount: favouritePda,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    
+    const account = await program.account.favouriteData.fetch(favouritePda);
+
+    console.log("Fetched account:", account);
+
+    
+    expect(account.number.toNumber()).to.equal(99);
+    expect(account.colour).to.equal("Blue");
+    expect(account.hobbies).to.deep.equal(["Coding", "Gym"]);
+    expect(account.animal).to.equal("Tiger");
   });
 });
